082c50a8f9f73625c8d5eed81ba78a0225ad2fa8
Sergio Rodriguez <sergio.rodriguezflores26@gmail.com>
2026-08-18 21:23:59 -0600

    working on a login page

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,14 @@
+
+
+export default function loginPage() {
+    return (
+        <>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username"></input>
+            <label for="password">password:</label>
+            <input type="passord" id="password" name="password"></input>
+        </>
+
+
+    );
+}
