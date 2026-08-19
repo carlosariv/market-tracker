30c0ee4b0b045ce2b49e6486d8e065ba6555ebe6
Carlos Rivera <61571282+carlosariv@users.noreply.github.com>
2026-08-19 15:03:57 -0500

    Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <div>
+            <footer>
+                <a>@ 2026 MarketWatch. All rights reserved.</a>
+                <a>Privacy</a>
+                <a>Terms</a>
+                <a>API Docs</a>
+                <a>Contact</a>
+            </footer>
+        </div>
+    )
+}
