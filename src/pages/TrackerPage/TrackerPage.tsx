a3aad21b7e538ad187405a887984e8ded9a5dd02
Carlos Rivera <61571282+carlosariv@users.noreply.github.com>
2026-08-20 10:46:31 -0500

    search bar, filters, and sorting buttons

--- src/pages/TrackerPage/TrackerPage.tsx
+++ src/pages/TrackerPage/TrackerPage.tsx
@@ -1,13 +1,69 @@
+import { useState } from "react";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import StockCard from "../../components/StockCard/StockCard";
 
 import "./TrackerPage.css"
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 export default function TrackerPage() {
+    const filterCategories : Array<string> = [
+        "All",
+        "Technology",
+        "Financials",
+        "Healthcare",
+        "Energy",
+        "Consumer",
+        "Industrials"
+    ];
+
+    const sortOptions : Array<string> = [
+        "Price",
+        "Change",
+        "Change %",
+        "Volume"
+    ];
+
+    const [searchQuery, setSearchQuery] = useState("");
+    const [sortOption, setSortOption] = useState("Price");
+
     return (
         <div>
             <Navbar />
+
+            <div className="market-overview">
+                <span style = {{fontWeight:600}}>Market Overview</span>
+                <span>API BUDGET 18/60 this minute</span>
+            </div>
+
+            <div className="filter-heading">
+                <div>
+                    <SearchBar
+                        value={searchQuery}
+                        placeholder="Filter by company"
+                        onSearch={(query) => {
+                            setSearchQuery(query);
+                        }}
+                    />
+
+                    <div className="filter-categories">
+                        {
+                            filterCategories.map(
+                                (s) => <button className="category">{s}</button>
+                            )
+                        }
+                    </div>
+                </div>
+
+                <select name="stock-sort" id="stock-sort-select"
+                    onChange={(e) => {
+                        console.log(e.target.value);
+                    }}
+                >
+                    {sortOptions.map((opt) => <option value={opt} key={opt}>{opt}</option>)};
+                </select>
+            </div>
+
             <div className="card-grid">
                 <StockCard symbol="AAPL" name="Apple inc." price={152.47}></StockCard>
                 <StockCard symbol="AAPL" name="Apple inc." price={152.47}></StockCard>
