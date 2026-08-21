4506c301543b43b9d29568437bff7bd8cd49d6ed
Carlos Rivera <61571282+carlosariv@users.noreply.github.com>
2026-08-20 21:15:38 -0500

    nav footer layout, Outlet for children of Route

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,27 @@
 import './Navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 export default function Navbar() {
 
   return (
-    <nav className="navbar">
-      <div className="nav-logo">Market Tracker</div>
-        <nav> <Link to="/markets">Markets</Link></nav>
-        <nav> <Link to="/stocks">Stocks</Link></nav>
-         <nav> <Link to="/login">Logout</Link></nav>
-    </nav>
+    <div>
+      <nav className="navbar">
+        <div className="nav-logo">Market Tracker</div>
+        <Link to="/markets">Markets</Link>
+        <Link to="/stocks">Stocks</Link>
+        <Link to="/login">Logout</Link>
+      </nav>
+
+      <Outlet/>
+
+      <footer>
+        <a>@ 2026 MarketWatch. All rights reserved.</a>
+        <a>Privacy</a>
+        <a>Terms</a>
+        <a>API Docs</a>
+        <a>Contact</a>
+      </footer>
+    </div>
   );
 }
 
