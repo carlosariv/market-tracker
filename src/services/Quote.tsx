6b62e6825384fc562ddacad739ea2b289fab33c7
Sergio Rodriguez <sergio.rodriguezflores26@gmail.com>
2026-08-20 18:11:53 -0600

    added stock quote search

---
+++ src/services/Quote.tsx
@@ -0,0 +1,41 @@
+// Get current price update
+// https://finnhub.io/docs/api/quote
+
+import finnhub from 'finnhub';
+import type { stockId } from './SymbolLookup';
+
+const API_KEY = import.meta.env.VITE_FINNHUB_API_KEY;
+const finnhubClient = new finnhub.DefaultApi(API_KEY);
+
+export interface quote {
+  "c": number,
+  "h": number,
+  "l": number,
+  "o": number,
+  "pc": number,
+  "t": number 
+}
+
+
+function getQuoteRequest(symbol:string): Promise<quote> {
+    return new Promise((resolve, reject) => {
+        finnhubClient.quote(symbol, {}, (error: any, data: any, response: any) => {
+            if (error) {
+                reject(error);        
+            } else {
+                resolve(data);
+            }
+        });
+    })
+}
+
+export async function getQuote(symbol:string) : Promise<quote> {
+    try {
+        const data = await getQuoteRequest(symbol)
+        return data
+    } catch (error) {
+        console.error('Quote search failed:', error);
+        throw error;
+    }
+    
+}
