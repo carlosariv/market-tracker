8aa744c79bba830aa206780b60c67fba8f8bd0eb
Carlos Rivera <carlosariv@outlook.com>
2026-08-24 14:10:31 -0500

    use authcontext

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,21 +1,24 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import './LoginPage.css'
+import { useAuth } from "../../components/AuthContext";
 
 export default function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const { login } = useAuth();
   const navigate = useNavigate();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault(); // stop the browser's default submit (page reload)
 
-    if (username === "admin" && password === "admin") {
-      localStorage.setItem("authenticated", "true");
+    let authenticated = login(username, password);
+
+    if (authenticated) {
       navigate('/markets')
     } else {
-      setError("Invalid username or password");
+      setError("Invalid username or password.");
     }
   };
 
