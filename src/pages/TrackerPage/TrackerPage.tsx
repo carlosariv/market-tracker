fcc094c5b609d093f3fd86c9095da0e402b79655
Carlos Rivera <61571282+carlosariv@users.noreply.github.com>
2026-08-27 10:20:52 -0500

    filter+sort page-visible cards. actual pagination

--- src/pages/TrackerPage/TrackerPage.tsx
+++ src/pages/TrackerPage/TrackerPage.tsx
@@ -9,16 +9,19 @@ import { getPeers } from "../../services/FindPeers";
 import { searchCompanyProfile } from "../../services/CompanyProfile";
 import { useSearchResults } from "../../context/Context";
 
-
-export default function TrackerPage() {
+export function TrackerPage() {
+    // TODO: Make categories map to specific industries when filtering. e.g Technology emcompasses Bio-Technology.
+    // Or does finnhub have a option for that?
     const filterCategories: Array<string> = [
         "All",
+        "Banking",
         "Technology",
-        "Financials",
-        "Healthcare",
+        "Financial Services",
+        "Health Care",
         "Energy",
-        "Consumer",
-        "Industrials"
+        "Retail",
+        "Industrials",
+        "Other"
     ];
 
     const sortOptions: Array<string> = [
@@ -28,34 +31,26 @@ export default function TrackerPage() {
         "Volume"
     ];
 
-    const [searchQuery, setSearchQuery] = useState("");
     const [sortOption, setSortOption] = useState("Price");
     const [filterCategory, setFilterCategory] = useState("All");
     const [testStock, setTestStock] = useState<stockId | null>(null);
     const [testQuote, setTestQuote] = useState<Quote | null>(null);
-
     const [currentPage, setCurrentPage] = useState(1);
+    const { stockCardMarkets, setStockCardMarkets, loadedStocks} = useSearchResults();
+
     const paginationSize = 3;
+    const cardsPerPage = 15;
+    const pagesAvailable = (loadedStocks.length / cardsPerPage) + 1;
+    const cardIndexStart = cardsPerPage * (currentPage - 1);
+    const cardIndexEnd   = Math.min(cardIndexStart + cardsPerPage, loadedStocks.length - 1);
 
-    useEffect(() => {
-        const loadTestCard = async () => {
-            try {
-                const results = await searchStockSymbol('apple');
-                const stockId = results[0];                        // one stockId
-                const quote = await getQuote(stockId.symbol);      // one Quote
-                setTestStock(stockId);
-                setTestQuote(quote);
-
-                console.log(stockId)
-                console.log(quote)
-            } catch (err) {
-                console.error('Failed to load test card:', err);
-            }
-        };
-        loadTestCard();
-    }, []);   // empty array = run once after first render
-
-    let maxPages = 10;
+    const visibleCards: StockCardProps[] = loadedStocks
+        .filter((card) => filterCategory=="All" || (card.companyProfile?.finnhubIndustry===filterCategory))
+        .sort((a: StockCardProps, b: StockCardProps) : number => {
+            // TODO: Sort handlers
+            return b.quote.pc - a.quote.pc;
+        })
+        .slice(cardIndexStart, cardIndexEnd);
 
     return (
         <div>
@@ -91,7 +86,7 @@ export default function TrackerPage() {
                     name="stock-sort"
                     id="stock-sort-select"
                     onChange={(e) => {
-                        console.log(e.target.value);
+                        setSortOption(e.target.value);
                     }}
                 >
                     {
@@ -101,151 +96,24 @@ export default function TrackerPage() {
             </div>
 
             <div className="card-grid">
-                {testStock && testQuote && (
-                    <>
-                        <StockCard stockId={testStock} quote={testQuote} />
-                        <StockCard stockId={testStock} quote={testQuote} />
-                        <StockCard stockId={testStock} quote={testQuote} />
-                        <StockCard stockId={testStock} quote={testQuote} />
-                    </>
-                )}
+                {
+                    visibleCards.map((card) => (
+                        <StockCardCompanyProfile key={card.stockId.symbol} stockId={card.stockId} companyProfile={card.companyProfile} quote={card.quote} />
+                    ))
+                }
+
             </div>
 
             <div className="d-center">
-                {
-                    Array.from({ length: paginationSize }, (_, index) => Math.max(1, currentPage - 1) + index)
-                        .map((page) =>
-                            <button
-                                key={page}
-                                onClick={(e) => {
-                                    setCurrentPage(page);
-                                    console.log(page);
-                                }}
-                                className={`btn ${page == currentPage ? "active-btn" : ""}`}
-                            >{page}</button>)
-                }
-            </div>
-        </div>
-    )
-}
-
-
-export function TrackerPageV2() {
-    const filterCategories: Array<string> = [
-        "All",
-        "Technology",
-        "Financials",
-        "Healthcare",
-        "Energy",
-        "Consumer",
-        "Industrials"
-    ];
-
-    const sortOptions: Array<string> = [
-        "Price",
-        "Change",
-        "Change %",
-        "Volume"
-    ];
-
-    const [searchQuery, setSearchQuery] = useState("");
-    const [sortOption, setSortOption] = useState("Price");
-    const [filterCategory, setFilterCategory] = useState("All");
-    const [testStock, setTestStock] = useState<stockId | null>(null);
-    const [testQuote, setTestQuote] = useState<Quote | null>(null);
-
-    const [currentPage, setCurrentPage] = useState(1);
-    const paginationSize = 3;
-    const { stockCardMarkets, setStockCardMarkets } = useSearchResults();
-    useEffect(() => {
-        const loadTestCard = async () => {
-            // console.log(stockCardMarkets['APPL'].forEach((p) => console.log(p)))
-            console.log(stockCardMarkets['PLTR'])
-            if (!stockCardMarkets['PLTR']) {
-                try {
-                    console.log("Loading apple peers")
-                    const peers = await getPeers('PLTR')
-                    console.log(peers)
-
-
-                    const stockCards: StockCardProps[] = []
-
-                    // Collect all relavant data for each stock card
-                    for (const p of peers) {
-                        stockCards.push({
-                            stockId: { description: "", displaySymbol: "", symbol: p, type: "" },
-                            companyProfile: await searchCompanyProfile(p),
-                            quote: await getQuote(p),
-                        });
+                <button 
+                    className="btn"
+                    onClick={(e) => {
+                        if (currentPage > 1) {
+                            setCurrentPage(prev => prev-1);
+                        }
                     }
+                }> L </button>
 
-                    // Getting variables from context and setting 
-
-                    stockCardMarkets['PLTR'] = stockCards
-                    setStockCardMarkets(prev => ({ ...prev, PLTR: stockCards }));
-                } catch (err) {
-                    console.error('Failed to load test card:', err);
-                }
-            }
-
-        };
-        loadTestCard();
-    }, []);   // empty array = run once after first render
-
-    let maxPages = 10;
-
-    return (
-        <div>
-            <div className="market-overview">
-                <span style={{ fontWeight: 600, fontSize: "24px" }}>Market Overview</span>
-                <span>API BUDGET 18/60 this minute</span>
-            </div>
-
-            <div className="filter-heading">
-                <div>
-                    <div className="filter-categories">
-                        {
-                            filterCategories.map(
-                                (category) => {
-                                    return (
-                                        <button
-                                            className={`btn ${filterCategory == category ? "active-btn" : ""}`}
-                                            key={category}
-                                            value={category}
-                                            onClick={(e) => {
-                                                console.log(e.currentTarget.value);
-                                                setFilterCategory(e.currentTarget.value);
-                                            }}
-                                        >{category}</button>
-                                    );
-                                }
-                            )
-                        }
-                    </div>
-                </div>
-
-                <select
-                    name="stock-sort"
-                    id="stock-sort-select"
-                    onChange={(e) => {
-                        console.log(e.target.value);
-                    }}
-                >
-                    {
-                        sortOptions.map((opt) => <option value={opt} key={opt}>Sort: {opt}</option>)
-                    };
-                </select>
-            </div>
-
-            <div className="card-grid">
-
-                {(stockCardMarkets['PLTR'] ?? []).map((p) => (
-                    <StockCardCompanyProfile key={p.stockId.symbol} stockId={p.stockId} companyProfile={p.companyProfile} quote={p.quote} />
-                ))}
-
-            </div>
-
-            <div className="d-center">
                 {
                     Array.from({ length: paginationSize }, (_, index) => Math.max(1, currentPage - 1) + index)
                         .map((page) =>
@@ -253,11 +121,19 @@ export function TrackerPageV2() {
                                 key={page}
                                 onClick={(e) => {
                                     setCurrentPage(page);
-                                    console.log(page);
                                 }}
                                 className={`btn ${page == currentPage ? "active-btn" : ""}`}
                             >{page}</button>)
                 }
+
+                <button
+                    className="btn"
+                    onClick={(e) => {
+                        if (currentPage < pagesAvailable - 1) {
+                            setCurrentPage(prev => prev + 1);
+                        }
+                    }
+                }> R </button>
             </div>
         </div>
     )
